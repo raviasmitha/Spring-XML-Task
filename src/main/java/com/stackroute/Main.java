083fc06713690adcd40586d6454a9e6a30b0b1d3
raviasmitha <asmithachowdary1511@gmail.com>
2019-07-11 15:37:15 +0530

    task2

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -14,14 +14,30 @@ import org.springframework.core.io.ClassPathResource;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
-        Movie movie = (Movie) applicationContext.getBean("movie");
+        Movie movie = (Movie) applicationContext.getBean("movie1");
         System.out.println(movie.getActor());
 
-        XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(new ClassPathResource("bean.xml"));
-        Movie movieFirst = (Movie) xmlBeanFactory.getBean("movie");
+       Movie movieFirst = applicationContext.getBean("movie2",Movie.class);
 
         System.out.println(movieFirst.getActor());
+        System.out.print("singleton scope: ");
 
+        System.out.println(movie==movieFirst);
+
+        Movie movie1 = applicationContext.getBean("movie3",Movie.class);
+        Movie movie2 = applicationContext.getBean("movie3",Movie.class);
+        System.out.println(movie1.getActor());
+        System.out.println(movie2.getActor());
+        System.out.print("scope prototype:");
+        System.out.println(movie1==movie2);
+
+        Movie movie3 = applicationContext.getBean("movie4",Movie.class);
+        Movie movie4 = applicationContext.getBean("movie4",Movie.class);
+        System.out.println(movie3.getActor());
+        System.out.println(movie4.getActor());
+
+        System.out.print("default scope:");
+        System.out.println(movie3==movie4);
     }
 
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -2,9 +2,13 @@ package com.stackroute.domain;
 
 public class Movie {
 
-    com.stackroute.domain.Actor actor;
+    Actor actor;
 
-    public void setActor(com.stackroute.domain.Actor actor) {
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
+
+    public void setActor(Actor actor) {
         this.actor = actor;
     }
 
